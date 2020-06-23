9b7ece7fa81f59bfe7e88ddd9343c8fe1b62eb70
raymondngiam <ha.ngiam87@gmail.com>
2020-06-23 19:04:20 +0800

    update main with example usages

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,65 @@
 
 int main()
 {
-    Pointer<int> p = new int(19);
-    p = new int(21);
-    p = new int(28);
+    std::cout<<"Non array example:"<<std::endl;
+    std::cout<<"=================="<<std::endl;
+    auto ptr1 = new int(19);
+    auto ptr2 = new int(21);
+    auto ptr3 = new int(28);
+    
+    // scope 1
+    if (true){
+        //ctor
+        Pointer<int> p(ptr1);
+        std::cout<<"ctor"<<std::endl;
+        Pointer<int>::showlist();
+
+        // scope 2
+        if (true){
+            //copy constructor
+            Pointer<int> p2(p);
+            std::cout<<"copy constructor"<<std::endl;
+            Pointer<int>::showlist();
+
+            //assignment of pointer to Pointer
+            p = ptr2;
+            p = ptr3;
+            std::cout<<"assignment of pointer to Pointer"<<std::endl;
+            Pointer<int>::showlist();
+        }
+        // end of scope 2: Pointer p2 should be disposed
+        std::cout<<"end of scope 2"<<std::endl;
+        Pointer<int>::showlist();
+    }
+    // end of scope 1: Pointer p1 should be disposed
+    std::cout<<"end of scope 1"<<std::endl;
+    Pointer<int>::showlist();
+
+    std::cout<<"Array example:"<<std::endl;
+    std::cout<<"=============="<<std::endl;
+    const int size = 3;
+    auto ptr4 = new std::string[size]{"abc","def","ghi"};
+
+    // scope 1
+    if (true){
+        //ctor
+        Pointer<std::string,size> p3(ptr4);
+        std::cout<<"ctor"<<std::endl;
+        Pointer<std::string,size>::showlist();
+        std::cout<<"Array content:"<<std::endl;
+        for (auto it = p3.begin(); it != p3.end(); it++)
+        {
+            std::cout<<*it<<std::endl;
+        }
+        std::cout<<"Array content (alternative):"<<std::endl;
+        for (const auto& item : p3)
+        {
+            std::cout<<item<<std::endl;
+        }
+    }
+    // end of scope 1: Pointer p3 should be disposed
+    std::cout<<"end of scope 1"<<std::endl;
+    Pointer<std::string,size>::showlist();
 
     return 0;
 }
