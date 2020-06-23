3e476fdd50c92af9e475f82265e5546abe946c9e
raymondngiam <ha.ngiam87@gmail.com>
2020-06-23 15:15:55 +0800

    amend comment on operator== function

--- src/gc_details.hpp
+++ src/gc_details.hpp
@@ -1,5 +1,7 @@
 // Overloading operator== allows two class objects to be compared.
-// This is needed by the STL list class.
+// This is needed when we are iterating through STL list to look 
+// for a specific ptr in the `Pointer<T, size>::findPtrInfo(T *ptr)`
+// function.
 template <class T>
 bool operator==(const PtrDetails<T> &ob1,
                 const PtrDetails<T> &ob2)
